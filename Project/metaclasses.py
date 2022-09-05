63a7c49885863906274a3fe1ab5fced0ed1a65cd
serg-myst <serg_myst@mail.ru>
2022-09-05 14:48:55 +0300

    Lesson_5

--- Project/metaclasses.py
+++ Project/metaclasses.py
@@ -1,99 +1,63 @@
-# Lesson_2
-
-# Библиотека dis позволяет разобрать код на атрибуты, методы, классы и т.д.
-# Описание: https://docs.python.org/3/library/dis.html
 import dis
 
 
-def create_lists(attrs):
-    # LOAD_GLOBAL - обычные методы, которые используются в фукнциях класса
-    # LOAD_METHOD - обычные меотды, обернутые декоратором
-    # LOAD_ATTR - атрибуты, используемые в функциях класса
+# Метакласс для проверки соответствия сервера:
+class ServerMaker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        # Список методов, которые используются в функциях класса:
+        methods = []
+        # Атрибуты, вызываемые функциями классов
+        attrs = []
+        for func in clsdict:
+            # Пробуем
+            try:
+                ret = dis.get_instructions(clsdict[func])
+                # Если не функция то ловим исключение
+            except TypeError:
+                pass
+            else:
+                # Раз функция разбираем код, получая используемые методы и атрибуты.
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+                    elif i.opname == 'LOAD_ATTR':
+                        if i.argval not in attrs:
+                            attrs.append(i.argval)
+        # Если обнаружено использование недопустимого метода connect, бросаем исключение:
+        if 'connect' in methods:
+            raise TypeError('Использование метода connect недопустимо в серверном классе')
+        # Если сокет не инициализировался константами SOCK_STREAM(TCP) AF_INET(IPv4), тоже исключение.
+        if not ('SOCK_STREAM' in attrs and 'AF_INET' in attrs):
+            raise TypeError('Некорректная инициализация сокета.')
+        super().__init__(clsname, bases, clsdict)
 
-    methods_load_global = []
-    methods_load_method = []
-    attributes = []
 
-    for f in attrs:
-        try:
-            res = dis.get_instructions(attrs[f])
-        except TypeError:
+# Метакласс для проверки корректности клиентов:
+class ClientMaker(type):
+    def __init__(cls, clsname, bases, clsdict):
+        # Список методов, которые используются в функциях класса:
+        methods = []
+        for func in clsdict:
+            # Пробуем
+            try:
+                ret = dis.get_instructions(clsdict[func])
+                # Если не функция то ловим исключение
+            except TypeError:
+                pass
+            else:
+                # Раз функция разбираем код, получая используемые методы.
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+        # Если обнаружено использование недопустимого метода accept, listen, socket бросаем исключение:
+        for command in ('accept', 'listen', 'socket'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        # Вызов get_message или send_message из utils считаем корректным использованием сокетов
+        if 'get_message' in methods or 'send_message' in methods:
             pass
         else:
-            for r in res:  # обход полученных атрибутов
-                if r.opname == 'LOAD_GLOBAL':
-                    if r.argval not in methods_load_global:
-                        methods_load_global.append(r.argval)
-                elif r.opname == 'LOAD_METHOD':
-                    if r.argval not in methods_load_method:
-                        methods_load_method.append(r.argval)
-                elif r.opname == 'LOAD_ATTR':
-                    if r.argval not in attributes:
-                        attributes.append(r.argval)
-
-    return methods_load_global, methods_load_method, attributes
-
-
-class ServerVerifier(type):
-
-    def __init__(cls, future_class_name, future_class_parent, future_class_attrs):
-        # Разложим по массивам методы и атрибуты. Потом проанализируем.
-
-        # Проверка зна сокеты TCP
-        check_tcp_sockets = ['AF_INET', 'SOCK_STREAM']
-        check_methods = ['connect']
-
-        methods_load_global, methods_load_method, attributes = create_lists(future_class_attrs)
-
-        # Проверка использования сокетов для работы по TCP
-        check_list = list(filter(lambda i: i in attributes, check_tcp_sockets))
-        if check_list != check_tcp_sockets:
-            raise RuntimeError('Для работы по TCP должны использоваться "AF_INET", "SOCK_STREAM"')
-
-        # Проверка на наличие методов connect
-        check_list = list(filter(lambda i: i in methods_load_global, check_methods))
-        if len(check_list) != 0:
-            raise RuntimeError('Для сервера не допустимо наличие методов "connect"')
-
-        # Для отладки оставлю.
-
-        # print(20 * '=')
-        # print(methods_load_global)
-        # print(20 * '=')
-        # print(methods_load_method)
-        # print(20 * '=')
-        # print(attributes)
-
-        super().__init__(future_class_name, future_class_parent, future_class_attrs)
-
-
-class ClientVerifier(type):
-
-    def __init__(cls, future_class_name, future_class_parent, future_class_attrs):
-        # Проверка зна сокеты TCP
-        check_tcp_sockets = ['sock']  # Уже содержит transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        check_methods = ['accept', 'listen']
-
-        methods_load_global, methods_load_method, attributes = create_lists(future_class_attrs)
-
-        # Проверка использования сокетов для работы по TCP
-        check_list = list(filter(lambda i: i in attributes, check_tcp_sockets))
-        if check_list != check_tcp_sockets:
-            raise RuntimeError('Для работы по TCP должны использоваться "AF_INET", "SOCK_STREAM"')
-
-        # Проверка на наличие методов accept, listen
-        check_list = list(filter(lambda i: i in methods_load_global, check_methods))
-        if len(check_list) != 0:
-            raise RuntimeError('Для клиента не допустимо наличие методов "accept", "listen"')
-
-        # Для отладки оставлю.
-
-        # print(20 * '=')
-        # print(methods_load_global)
-        # print(20 * '=')
-        # print(methods_load_method)
-        # print(20 * '=')
-        # print(attributes)
-
-        super().__init__(future_class_name, future_class_parent, future_class_attrs)
-
+            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+        super().__init__(clsname, bases, clsdict)
--- Project/server_gui.py
+++ Project/server_gui.py
@@ -1,175 +1,134 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel, QTableView, QDialog, QPushButton, \
     QLineEdit, QFileDialog, QMessageBox
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt
 import os
-from server_storage import ServerStorage
 
 
+# GUI - Создание таблицы QModel, для отображения в окне программы.
+def gui_create_model(database):
+    list_users = database.active_users_list()
+    list_table = QStandardItemModel()
+    list_table.setHorizontalHeaderLabels(['Имя Клиента', 'IP Адрес', 'Порт', 'Время подключения'])
+    for row in list_users:
+        user, ip, port, time = row
+        user = QStandardItem(user)
+        user.setEditable(False)
+        ip = QStandardItem(ip)
+        ip.setEditable(False)
+        port = QStandardItem(str(port))
+        port.setEditable(False)
+        # Уберём миллисекунды из строки времени, т.к. такая точность не требуется.
+        time = QStandardItem(str(time.replace(microsecond=0)))
+        time.setEditable(False)
+        list_table.appendRow([user, ip, port, time])
+    return list_table
+
+
+# GUI - Функция реализующая заполнение таблицы историей сообщений.
+def create_stat_model(database):
+    # Список записей из базы
+    hist_list = database.message_history()
+
+    # Объект модели данных:
+    list_table = QStandardItemModel()
+    list_table.setHorizontalHeaderLabels(
+        ['Имя Клиента', 'Последний раз входил', 'Сообщений отправлено', 'Сообщений получено'])
+    for row in hist_list:
+        user, last_seen, sent, recvd = row
+        user = QStandardItem(user)
+        user.setEditable(False)
+        last_seen = QStandardItem(str(last_seen.replace(microsecond=0)))
+        last_seen.setEditable(False)
+        sent = QStandardItem(str(sent))
+        sent.setEditable(False)
+        recvd = QStandardItem(str(recvd))
+        recvd.setEditable(False)
+        list_table.appendRow([user, last_seen, sent, recvd])
+    return list_table
+
+
+# Класс основного окна
 class MainWindow(QMainWindow):
-    def __init__(self, db):
+    def __init__(self):
         super().__init__()
         self.initUI()
-        self.db = db
-        self.create_active_users()
 
     def initUI(self):
-        # Создаем кнопки в главном меню окна
-        # Кнопка "Выход"
-        self.exit = QAction(QIcon("exit.png"), 'Выход', self)
-        self.exit.setShortcut('Ctrl+Q')
-        self.exit.setStatusTip('Выход')
-        self.exit.triggered.connect(qApp.quit)
+        # Кнопка выхода
+        self.exitAction = QAction('Выход', self)
+        self.exitAction.setShortcut('Ctrl+Q')
+        self.exitAction.triggered.connect(qApp.quit)
 
-        # Кнопка обновить таблицу активных пользователей
-        self.refresh = QAction(QIcon("refresh.png"), 'Выход', self)
-        self.refresh.setShortcut('Ctrl+R')
-        self.refresh.setStatusTip('Обновить')
-        self.refresh.triggered.connect(self.create_active_users)
+        # Кнопка обновить список клиентов
+        self.refresh_button = QAction('Обновить список', self)
 
-        # Кнопка истории клиентов
-        self.show_history = QAction('История клиентов', self)
-        self.show_history.triggered.connect(self.create_history_table)
+        # Кнопка вывести историю сообщений
+        self.show_history_button = QAction('История клиентов', self)
 
-        # Кнопка настройки сервера
-        self.show_server_parameters = QAction('Настройки сервера', self)
-        self.show_server_parameters.triggered.connect(self.show_parameters)
+        # Кнопка настроек сервера
+        self.config_btn = QAction('Настройки сервера', self)
 
-        # Кнопка показать всех пользователей
-        self.show_all_users = QAction('Показать всех пользователей', self)
-        self.show_all_users.triggered.connect(self.create_all_users_table)
-
+        # Статусбар
+        # dock widget
         self.statusBar()
 
-        # Размеры окна + Заголовок
-        self.setFixedSize(800, 600)
-        self.setWindowTitle('Обработка работы сервера')
-
+        # Тулбар
         self.toolbar = self.addToolBar('MainBar')
-        self.toolbar.addAction(self.exit)
-        self.toolbar.addAction(self.refresh)
-        self.toolbar.addAction(self.show_history)
-        self.toolbar.addAction(self.show_server_parameters)
-        self.toolbar.addAction(self.show_all_users)
+        self.toolbar.addAction(self.exitAction)
+        self.toolbar.addAction(self.refresh_button)
+        self.toolbar.addAction(self.show_history_button)
+        self.toolbar.addAction(self.config_btn)
 
-        self.active_clients = QTableView(self)
-        self.active_clients.move(10, 55)
-        self.active_clients.setFixedSize(780, 400)
+        # Настройки геометрии основного окна
+        # Поскольку работать с динамическими размерами мы не умеем, и мало времени на изучение, размер окна фиксирован.
+        self.setFixedSize(800, 600)
+        self.setWindowTitle('Messaging Server alpha release')
 
+        # Надпись о том, что ниже список подключённых клиентов
+        self.label = QLabel('Список подключённых клиентов:', self)
+        self.label.setFixedSize(400, 15)
+        self.label.move(10, 35)
+
+        # Окно со списком подключённых клиентов.
+        self.active_clients_table = QTableView(self)
+        self.active_clients_table.move(10, 55)
+        self.active_clients_table.setFixedSize(780, 400)
+
+        # Последним параметром отображаем окно.
         self.show()
 
-    # Добавил еще и список всех пользователей
-    def create_all_users_table(self):
-        database = self.db
-        window = AllUsersWindow(self)
-        users_list = database.users_list()
-        list_table = QStandardItemModel(window)  # табличная часть
-        list_table.setHorizontalHeaderLabels(['Пользователь', 'Время последнего подключения'])
-        for user in users_list:
-            user_login = QStandardItem(user.login)
-            user_login.setEditable(False)
-            last_connection = QStandardItem(str(user.last_connection))
-            last_connection.setEditable(False)
-            list_table.appendRow([user_login, last_connection])
-        window.table_list.setModel(list_table)
-        window.table_list.resizeColumnsToContents()
-        window.exec_()
 
-    # Выведем историю
-    def create_history_table(self):
-        database = self.db
-        window = UsersHistoryWindow(self)
-        users_list = database.login_history()
-        list_table = QStandardItemModel(window)  # табличная часть
-        list_table.setHorizontalHeaderLabels(['Пользователь', 'Время последнего подключения'])
-        for user in users_list:
-            user_login = QStandardItem(user.login)
-            user_login.setEditable(False)
-            ip = QStandardItem(user.ip)
-            ip.setEditable(False)
-            port = QStandardItem(str(user.port))
-            port.setEditable(False)
-            login_time = QStandardItem(str(user.login_time))
-            login_time.setEditable(False)
-            list_table.appendRow([user_login, ip, port, login_time])
-        window.table_list.setModel(list_table)
-        window.table_list.resizeColumnsToContents()
-        window.exec_()
-
-    # Заполнение таблицы активных пользователей
-    def create_active_users(self):
-        database = self.db
-        users_list = database.active_users_list()
-        list_table = QStandardItemModel(self)  # табличная часть
-        list_table.setHorizontalHeaderLabels(['Клиент', 'IP', 'Порт', 'Время подключения'])
-        for user in users_list:
-            user_login = QStandardItem(user.login)
-            user_login.setEditable(False)
-            ip = QStandardItem(user.ip)
-            ip.setEditable(False)
-            port = QStandardItem(str(user.port))
-            port.setEditable(False)
-            login_time = QStandardItem(str(user.login_time))
-            login_time.setEditable(False)
-            list_table.appendRow([user_login, ip, port, login_time])
-        self.active_clients.setModel(list_table)
-        self.active_clients.resizeColumnsToContents()
-
-    # Открываем окно с настройками сервера
-    def show_parameters(self):
-        window = ConfigWindow(self)
-        window.exec_()
-
-
-class AllUsersWindow(QDialog):
-    def __init__(self, parent=None):
-        super(AllUsersWindow, self).__init__(parent)
+# Класс окна с историей пользователей
+class HistoryWindow(QDialog):
+    def __init__(self):
+        super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Список всех пользователей')
+        # Настройки окна:
+        self.setWindowTitle('Статистика клиентов')
         self.setFixedSize(600, 700)
         self.setAttribute(Qt.WA_DeleteOnClose)
 
+        # Кнопка закрытия окна
         self.close_button = QPushButton('Закрыть', self)
         self.close_button.move(250, 650)
         self.close_button.clicked.connect(self.close)
 
         # Лист с собственно историей
-        self.table_list = QTableView(self)
-        self.table_list.move(10, 10)
-        self.table_list.setFixedSize(580, 620)
+        self.history_table = QTableView(self)
+        self.history_table.move(10, 10)
+        self.history_table.setFixedSize(580, 620)
 
         self.show()
 
 
-class UsersHistoryWindow(QDialog):
-    def __init__(self, parent=None):
-        super(UsersHistoryWindow, self).__init__(parent)
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('История пользователей')
-        self.setFixedSize(600, 700)
-        self.setAttribute(Qt.WA_DeleteOnClose)
-
-        self.close_button = QPushButton('Закрыть', self)
-        self.close_button.move(250, 650)
-        self.close_button.clicked.connect(self.close)
-
-        # Лист с собственно историей
-        self.table_list = QTableView(self)
-        self.table_list.move(10, 10)
-        self.table_list.setFixedSize(580, 620)
-
-        self.show()
-
-
-# Формируем окно с настройками сервера
+# Класс окна настроек
 class ConfigWindow(QDialog):
-    def __init__(self, parent=None):
-        super(ConfigWindow, self).__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.initUI()
 
     def initUI(self):
@@ -249,28 +208,37 @@ class ConfigWindow(QDialog):
         self.show()
 
 
-def main(db):
-    app = QApplication(sys.argv)
-    main_window = MainWindow(db)
-    main_window.statusBar().showMessage('Панель управления...')
-    main_window.create_active_users()
-    sys.exit(app.exec_())
-
 if __name__ == '__main__':
-    # create_all_users_table()
-
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    main_window.statusBar().showMessage('тестирование...')
-    main_window.create_active_users()
+    main_window.statusBar().showMessage('Test Statusbar Message')
+    test_list = QStandardItemModel(main_window)
+    test_list.setHorizontalHeaderLabels(['Имя Клиента', 'IP Адрес', 'Порт', 'Время подключения'])
+    test_list.appendRow(
+        [QStandardItem('test1'), QStandardItem('192.198.0.5'), QStandardItem('23544'), QStandardItem('16:20:34')])
+    test_list.appendRow(
+        [QStandardItem('test2'), QStandardItem('192.198.0.8'), QStandardItem('33245'), QStandardItem('16:22:11')])
+    main_window.active_clients_table.setModel(test_list)
+    main_window.active_clients_table.resizeColumnsToContents()
+    app.exec_()
 
-    '''
-    test_list = QStandardItemModel(main_window)  # табличная часть
-    test_list.setHorizontalHeaderLabels(['Клиент', 'IP', 'Порт', 'Время подключения'])
-    test_list.appendRow([QStandardItem('test'), QStandardItem('192.168.0.1'), QStandardItem('7777'),
-                         QStandardItem('16:15:00')])
-    main_window.active_clients.setModel(test_list)
-    main_window.active_clients.resizeColumnsToContents()
-    '''
+    # ----------------------------------------------------------
+    # app = QApplication(sys.argv)
+    # window = HistoryWindow()
+    # test_list = QStandardItemModel(window)
+    # test_list.setHorizontalHeaderLabels(
+    #     ['Имя Клиента', 'Последний раз входил', 'Отправлено', 'Получено'])
+    # test_list.appendRow(
+    #     [QStandardItem('test1'), QStandardItem('Fri Dec 12 16:20:34 2020'), QStandardItem('2'), QStandardItem('3')])
+    # test_list.appendRow(
+    #     [QStandardItem('test2'), QStandardItem('Fri Dec 12 16:23:12 2020'), QStandardItem('8'), QStandardItem('5')])
+    # window.history_table.setModel(test_list)
+    # window.history_table.resizeColumnsToContents()
+    #
+    # app.exec_()
 
-    sys.exit(app.exec_())
+    # ----------------------------------------------------------
+    # app = QApplication(sys.argv)
+    # dial = ConfigWindow()
+    #
+    # app.exec_()
